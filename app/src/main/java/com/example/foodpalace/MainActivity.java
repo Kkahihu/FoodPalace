defbdcef3968f617725f2b1418a415bdb0b356ce
Kelvin Kahihu <kahihu35@gmai.com>
2020-08-05 11:41:35 +0300

    initial commit

---
+++ app/src/main/java/com/example/foodpalace/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.foodpalace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import info.hoang8f.widget.FButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSignIn = (FButton)findViewById(R.id.btnSignIn);
+        btnSignUp = (FButton)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/NABILA.TTF");
+        txtSlogan.setTypeface(typeface);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(MainActivity.this, SignIn.class);
+                startActivity(signIn);
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
